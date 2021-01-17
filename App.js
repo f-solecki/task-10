bb335e606780f01a1ac5b06b58cf8710d0df13b6
soolecki <filipsolecki@gmail.com>
2021-01-17 11:38:34 +0100

    Added loading view

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import { FlatList, Text, View, Image } from 'react-native';
+import { FlatList, Text, View, Image, LogBox } from 'react-native';
 
 export default class App extends Component {
   constructor(props) {
@@ -8,6 +8,7 @@ export default class App extends Component {
 
     this.state = {
       data: [],
+      loading: true
     };
   }
 
@@ -15,24 +16,29 @@ export default class App extends Component {
     fetch('https://jsonplaceholder.typicode.com/photos')
       .then((response) => response.json())
       .then((json) => {
-        this.setState({ data: json });
+        this.setState({ data: json, loading: false });
       })
       .catch((error) => console.error(error))
   }
 
+
   render() {
-    const { data } = this.state;
+    LogBox.ignoreAllLogs("Warning: Can't perform a React state update on an unmounted component.This is a no - op, but it indicates a memory leak in your application.To fix, cancel all subscriptions and asynchronous tasks in the componentWillUnmount method.")
+    const { data, loading } = this.state;
     return (
-      <View style={{ flex: 1, marginTop: 45 }}>
-        <FlatList
-          data={data}
-          keyExtractor={({ id }) => id.toString()}
-          renderItem={({ item }) => (<View style={{ marginBottom: 20, borderWidth: 1, borderColor: 'black', alignItems: 'center', padding: 5, marginLeft: 5, marginRight: 5 }}>
-            <Text style={{ fontSize: 15, fontWeight: 'bold' }}>{item.title}</Text>
-            <Image source={{ uri: item.thumbnailUrl }} style={{ width: 150, height: 150 }} />
-          </View>
-          )}
-        />
+      <View style={{ flex: 1, marginTop: 45, justifyContent: loading == true ? 'center' : 'flex-start' }}>
+        {loading == true ? <Text style={{ alignSelf: 'center' }}>Please wait, data is loading.{'\n'}It can take a while.</Text> :
+
+          <FlatList
+            data={data}
+            keyExtractor={({ id }) => id.toString()}
+            renderItem={({ item }) => (<View style={{ marginBottom: 20, borderWidth: 1, borderColor: 'black', alignItems: 'center', padding: 5, marginLeft: 5, marginRight: 5 }}>
+              <Text style={{ fontSize: 15, fontWeight: 'bold' }}>{item.title}</Text>
+              <Image source={{ uri: item.thumbnailUrl }} style={{ width: 150, height: 150 }} />
+            </View>
+            )}
+          />
+        }
       </View>
     );
   }
